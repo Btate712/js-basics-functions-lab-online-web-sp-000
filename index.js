87ab2eca775c3492ddf4780afb666bad7d448e76
Robert Tate <btate712@gmail.com>
2019-06-02 02:34:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function distanceTravelledInFeet(beginningBlock, endingBlock) {
   blocksTravelled = Math.abs(beginningBlock - endingBlock);
   return blocksTravelled * 264;
 }
+
+function distanceFromHqInBlocks(streetLocation) {
+  return Math.abs(streetLocation - 42);
+}
