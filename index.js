cadc9beff446f69e7a1b64a52bbbaa9e693bbf0c
Robert Tate <btate712@gmail.com>
2019-06-02 02:47:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceTravelledInFeet(beginningBlock, endingBlock) {
+  let blocksTravelled = Math.abs(beginningBlock - endingBlock);
+  return blocksTravelled * 264;
+}
+
+function distanceFromHqInBlocks(streetLocation) {
+  return Math.abs(streetLocation - 42);
+}
+
+function distanceFromHqInFeet(streetLocation) {
+  return 264 * distanceFromHqInBlocks(streetLocation);
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination);
+  if (distance < 400) {
+    return 0;
+  }
+  else if (distance >= 400 && distance < 2000) {
+    return parseFloat((0.02 * distance - 8).toFixed(2));
+  }
+  else if (distance >= 2000 && distance < 2500) {
+    return 25;
+  }
+  else {
+    return 'cannot travel that far'
+  }
+}
