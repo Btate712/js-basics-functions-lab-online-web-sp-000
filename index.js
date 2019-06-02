164998e4faaa12e2893e9a2dbbc8e1e5b58b3319
Robert Tate <btate712@gmail.com>
2019-06-02 02:30:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution in this file!
+function distanceTravelledInFeet(beginningBlock, endingBlock) {
+  blocksTravelled = Math.abs(beginningBlock - endingBlock);
+  return blocksTravelled * 264;
+}
