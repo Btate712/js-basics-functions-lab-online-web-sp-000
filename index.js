aa46c6725802ab44e64251b73277872b89849fb5
Robert Tate <btate712@gmail.com>
2019-06-02 02:41:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ function distanceFromHqInBlocks(streetLocation) {
 function distanceFromHqInFeet(streetLocation) {
   return 264 * distanceFromHqInBlocks(streetLocation);
 }
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination);
+  if distance < 400 {
+    return 0;
+  } 
+  else if distance >= 400 && distance < 2000 {
+    return 0.02 * distance;
+  }
+  else if distance >= 2000 && distance < 2500 {
+    return 25;
+  }
+  else if distance >= 2500 {
+    return 'cannot travel that far'
+  }
+}
